e9c8ff00db01fc7d822be57ce2b909c53ba7c927
Daniel Criado <danicri96@hotmail.com>
2019-06-28 23:01:54 +0200

    Comprobar que al menos se selecciona 1 asig en el planificador

--- HorarioApp/public/js/seleccionAsignaturas.js
+++ HorarioApp/public/js/seleccionAsignaturas.js
@@ -1,14 +1,24 @@
 /* eslint-disable func-names */
 
+
 /**
  * Script que hace que las tablas de asignaturas sea dinámicas, quitando la
  * asignatura sobre la que se hace click de una tabla y añadiédola a la otra.
  *
  * Además, recoge la selección del usuario (un JSON con las asignaturas elegidas)
- * para posteriormente enviársela al servidor mediante una petición POST con AJAX.
+ * para posteriormente enviársela al servidor mediante una petición POST.
  */
 $(document).ready(() => {
   const asignaturasSelec = [];
+
+  // Función que comprueba que al menos hay una asignatura seleccionada.
+  // Desactiva el botón para continuar en caso de que no haya ninguna.
+  const checkAsigSeleccionada = () => {
+    const emptyAsignaturas = asignaturasSelec.length === 0; // check if at least one checked
+    $('#confirmar').prop('disabled', emptyAsignaturas);
+  };
+
+  checkAsigSeleccionada(); // Comprobación inicial
 
   // Si hace click en una fila de la tabla de disponibles
   $('tr.dispTableRow').click(function () {
@@ -18,6 +28,7 @@ $(document).ready(() => {
     $(this).fadeOut('slow', () => {
       $(`#selec${id}`).fadeIn('slow');
     });
+    checkAsigSeleccionada();
   });
 
   // Función auxiliar para buscar y quitar el codigo de la asignatura
@@ -39,6 +50,7 @@ $(document).ready(() => {
     $(this).fadeOut('slow', () => {
       $(`#disp${id}`).fadeIn('slow');
     });
+    checkAsigSeleccionada();
   });
 
   // Cambiamos el valor del input antes de enviar el formulario
